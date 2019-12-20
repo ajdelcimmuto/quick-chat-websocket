0e0bc1f67b48ccb43904de944827e1ca631fc913
imverbatim <ajdelco22@gmail.com>
2019-12-19 21:42:31 -0600

    changes to appjs

--- public/app.js
+++ public/app.js
@@ -12,7 +12,7 @@ new Vue({
 
     created: function() {
         var self = this;
-        this.ws = new WebSocket('ws://' + window.location.host + '/ws');
+        this.ws = new WebSocket(location.protocol.replace("http","ws") + "//" + location.host + "/ws");
         this.ws.addEventListener('message', function(e) {
             var msg = JSON.parse(e.data);
             self.chatContent += '<div class="chip">'
